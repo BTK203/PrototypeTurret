bd18ad7f96e3f9dcab6b26ae2d19858efba93345
BTK203 <brach2003@gmail.com>
2020-01-09 21:06:56 -0600

    Added and confirmed manual control.

--- PrototypeTurret/src/main/java/frc/robot/Robot.java
+++ PrototypeTurret/src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,53 +7,67 @@
 
 package frc.robot;
 
+import java.sql.Driver;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import frc.robot.subsystems.SubsystemDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-
-  public static SubsystemDrive SUB_DRIVE;
-  public static OI oi;
+  private RobotContainer m_robotContainer;
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
    */
   @Override
   public void robotInit() {
     DriverStation.reportWarning("ROBOT INIT STARTING", false);
-
-    SUB_DRIVE = new SubsystemDrive();
-
-    //init oi last
-    oi = new OI();
-
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
     DriverStation.reportWarning("ROBOT INIT DONE", false);
   }
 
   /**
-   * Called every time scheduler runs
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    Scheduler.getInstance().run();
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
 
   /**
-   * Called when auto starts
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /**
+   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
    */
   @Override
   public void autonomousInit() {
-    DriverStation.reportWarning("OH LAWD HE COMIN", false);
   }
 
   /**
@@ -61,7 +75,11 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopInit() {
+    CommandScheduler.getInstance().run();
   }
 
   /**
@@ -69,7 +87,12 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
   }
 
   /**
@@ -77,6 +100,5 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
-    Scheduler.getInstance().run();
   }
 }
--- PrototypeTurret/src/main/java/frc/robot/subsystems/SubsystemDrive.java
+++ PrototypeTurret/src/main/java/frc/robot/subsystems/SubsystemDrive.java
@@ -8,16 +8,17 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.commands.ManualCommandDrive;
 import frc.robot.util.Xbox;
 
 public class SubsystemDrive extends SubsystemBase {
-  private TalonSRX
+  
+  private TalonSRX 
     rightMaster,
     rightSlave,
     leftMaster,
@@ -32,7 +33,7 @@ public class SubsystemDrive extends SubsystemBase {
     leftMaster  = new TalonSRX(Constants.DRIVE_LEFT_MASTER_ID);
     leftSlave   = new TalonSRX(Constants.DRIVE_LEFT_SLAVE_ID);
 
-    setDefaultCommand(new ManualCommandDrive());
+    configureTalons();
   }
 
   @Override
@@ -40,7 +41,7 @@ public class SubsystemDrive extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public void driveTank(Joystick joy) {
+  public void drive(Joystick joy) {
     double throttle = Xbox.RT(joy) - Xbox.LT(joy);
     double steering = Xbox.LEFT_X(joy);
 
@@ -54,5 +55,17 @@ public class SubsystemDrive extends SubsystemBase {
     rightSlave.set(ControlMode.PercentOutput, right);
     leftMaster.set(ControlMode.PercentOutput, left);
     leftSlave.set(ControlMode.PercentOutput, left);
+  }
+
+  private void configureTalons() {
+    rightMaster.setInverted(Constants.DRIVE_RIGHT_MASTER_INVERT);
+    rightSlave.setInverted(Constants.DRIVE_RIGHT_SLAVE_INVERT);
+    leftMaster.setInverted(Constants.DRIVE_LEFT_MASTER_INVERT);
+    leftSlave.setInverted(Constants.DRIVE_LEFT_SLAVE_INVERT);
+
+    rightMaster.setNeutralMode(NeutralMode.Brake);
+    rightSlave.setNeutralMode(NeutralMode.Brake);
+    leftMaster.setNeutralMode(NeutralMode.Brake);
+    leftSlave.setNeutralMode(NeutralMode.Brake);
   }
 }
